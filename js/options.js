1533f3cccede1da563d76dffe51244c075a9d457
Antonio Rodriguez <antonio.rodriguez@udg.edu>
2026-03-05 21:46:46 +0100

    Opcions privades

--- js/options.js
+++ js/options.js
@@ -1,31 +1,42 @@
 import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 
-const default_options = {
-    pairs: 2,
-    difficulty: 'normal'
-} 
+var options = function(){
+    const default_options = {
+        pairs: 2,
+        difficulty: 'normal'
+    } 
 
-var pairs = $('#pairs');
-var difficulty = $('#dif');
+    var pairs = $('#pairs');
+    var difficulty = $('#dif');
 
-var options = Object.create(default_options);
+    var options = Object.create(default_options);
 
-pairs.on('change', function (){
-    options.pairs = pairs.val();
-});
+    pairs.on('change', function (){
+        options.pairs = pairs.val();
+    });
 
-difficulty.on('change', function (){
-    options.difficulty = difficulty.val();
-});
+    difficulty.on('change', function (){
+        options.difficulty = difficulty.val();
+    });
+
+    return {
+        applyChanges: function(){
+            // TODO: Save
+        },
+        defaultValues: function(){
+            options.pairs = default_options.pairs;
+            options.difficulty = default_options.difficulty;
+            pairs.val(options.pairs);
+            difficulty.val(options.difficulty);
+        }
+    }
+}();
 
 $('#default').on('click', function(){
-    options.pairs = default_options.pairs;
-    options.difficulty = default_options.difficulty;
-    pairs.val(options.pairs);
-    difficulty.val(options.difficulty);
+    options.defaultValues();
 })
 
 $('#apply').on('click', function(){
-    // TODO: Save
+    options.applyChanges();
     location.assign("../");
 });
