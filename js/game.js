6e7edf4127436c86b6c10526141a2ad63ea0972b
MaxBelfort <maxbelfort2005@gmail.com>
2026-03-15 20:16:57 +0100

    He tornat a modificar lo del jQuery perque no acabava de funcionar

--- js/game.js
+++ js/game.js
@@ -1,32 +1,27 @@
-import $ from "../library/jquery-4.0.0.slim.module.min.js";
+import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 import {clickCard, items, selectCards, startGame} from "./memory.js";
 
 var cards = [];
-var $game = $('#game');
+var game = $('#game');
 
 selectCards();
-
-$(items).each(function(idx, value) {
-    let $newCard = $('<img>', {
-        id: idx,
-        title: 'card'
-    });
-    
-    $game.append($newCard);
-    cards.push($newCard);
-    setValue(idx, value);
+items.forEach(function (value, idx)
+{
+    game.append(`<img id="${idx}" title="card">`);  // Add element
+    cards.push($(`#${idx}`));                       // Obtain element
+    setValue(idx, value);                           // Modify values
 });
 
-export function setValue(idx, value) {
+export function setValue(idx, value){
     cards[idx].attr('src', value);
 }
 
-export function clickOff(idx) {
+export function clickOff(idx){
     cards[idx].off('click');
 }
 
-export function clickOn(idx) {
-    cards[idx].on('click', function() {
+export function clickOn(idx){
+    cards[idx].on('click', function(){
         clickCard(idx);
     });
 }
--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-import $ from '../library/jquery-4.0.0.slim.module.min.js';
-
-$(function() {
+$(document).ready(function() {
+    
+    // Botón Play
     $('#play').on('click', function() {
         let nom = prompt("Nom del personatge:");
         console.log("Iniciant partida amb el nom: " + nom);
         alert("El teu nom de jugador és " + nom + "!");
-        $(location).attr('href', "./html/game.html");
+        
+        // En jQuery seguimos usando window.location para redirecciones
+        window.location.assign("./html/game.html");
     });
 
+    // Botón Options
     $('#options').on('click', function() {
         console.error("Opció no implementada");
     });
 
+    // Botón Saves
     $('#saves').on('click', function() {
         console.error("Opció no implementada");
     });
 
+    // Botón Exit
     $('#exit').on('click', function() {
         console.warn("No es pot sortir!");
     });
+
 });
